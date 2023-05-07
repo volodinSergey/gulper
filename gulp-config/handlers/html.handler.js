a79a80e786e8c80370f9df2718eee1079e92841d
Volodin Sergey <volodin7ergey@icloud.com>
2023-05-08 01:08:02 +0300

    [new plugin]: added gulp-replace plugin to fix images paths in html]

--- gulp-config/handlers/html.handler.js
+++ gulp-config/handlers/html.handler.js
@@ -1,6 +1,7 @@
 import gulp from 'gulp'
 import fileInclude from 'gulp-file-include'
 import browserSync from 'browser-sync'
+import replace from 'gulp-replace'
 
 import { pathsConfig } from '../paths.config.js'
 
@@ -8,5 +9,6 @@ const { src, dest } = gulp
 
 export const handleHtml = () => src(pathsConfig.src.html)
     .pipe(fileInclude())
+    .pipe(replace(/@images\//g, 'images/'))
     .pipe(dest(pathsConfig.build.html))
     .pipe(browserSync.stream())

c521bc8c522bb69667c7cbce26885a2130b589a0
Volodin Sergey <volodin7ergey@icloud.com>
2023-05-08 02:18:34 +0300

    [release/1.0.0 - docs]: added docs for images  handler

--- gulp-config/handlers/images.handler.js
+++ gulp-config/handlers/images.handler.js
@@ -5,6 +5,10 @@ import { pathsConfig } from '../paths.config.js'
 
 const { src, dest } = gulp
 
+/**
+ * @function handleImages
+ * @description Handler for images
+ */
 export const handleImages = () => src(pathsConfig.src.images)
     .pipe(dest(pathsConfig.build.images))
     .pipe(browserSync.stream())
